a545f43d6ba9edce7170d3d7cc585b47af56ab7e
KonradPietocha <konrad.pietocha@gazeta.pl>
2020-09-14 22:54:49 +0200

    change footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,4 @@
 import React from "react";
-//icons
-import gmail from '../../icons/gmail.ico';
-import linkedin from '../../icons/linkedin.jpg';
 //style
 import './footer.css';
 
@@ -10,18 +7,8 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <p>Web developer: KoNrAd PiĘtOcHa</p>
         <a href="mailto:konrad.pietocha80@gmail.com">
-          <img
-            src={gmail}
-            alt="gmail"
-          />
-        </a>
-        <a href="https://pl.linkedin.com/in/konrad-piętocha-48506b193">
-          <img
-            src={linkedin}
-            alt="linkedin"
-          />
+          Web developer: KoNrAd PiĘtOcHa
         </a>
       </footer>
     </>
