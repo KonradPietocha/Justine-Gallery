64cb14a0d694ed631e81b2316e5da0f5957d0c96
KonradPietocha <konrad.pietocha@gazeta.pl>
2023-04-07 11:40:00 +0200

    Updatedependencies

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import reducer from './redux/reducer';
+import { store } from './app/store';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-//style
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const destination = document.querySelector("#root");
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-const store = createStore(reducer);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  destination
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,10 +1,10 @@
 export const getState = state => {
     return {
-        menuOpen: state.menuOpener.menu,
-        menuSwitch: state.menuSwitch.content,
-        mobileSwitch: state.mobileSwitch.mobile,
-        dehazer: state.dehazer.menu,
-        indexer: state.indexer.index,
-        enlarger: state.enlarger.image
+        menuOpen: state.menuOpener?.menu,
+        menuSwitch: state.menuSwitch?.content,
+        mobileSwitch: state.mobileSwitch?.mobile,
+        dehazer: state.dehazer?.menu,
+        indexer: state.indexer?.index,
+        enlarger: state.enlarger?.image
     };
 };
